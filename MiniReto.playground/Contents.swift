4bc49edb7b15c77ad686cddac7046122db22cb3c
saragon <s_2118@hotmail.com>
2015-11-14 01:15:07 -0600

    Mini reto 2

--- MiniReto.playground/Contents.swift
+++ MiniReto.playground/Contents.swift
@@ -17,3 +17,35 @@ for var i = 0; i <= 100; i++ {
         print("\(i) Viva Swift");
     }
 }
+
+var numeros = 0...100
+var salida = ""
+
+for num in numeros {
+    
+    salida = "#\(num)"
+    
+    if( (num % 5) == 0){
+        salida += " - Bingo"
+    }
+    
+    if( (num % 2) == 0){
+        salida += " - Par"
+    }
+    
+    if( (num % 2) != 0){
+        salida += " - Impar"
+    }
+    
+    if( num >= 30 && num <= 40){
+        salida += " - Viva Swift"
+    }
+    
+    if( num == 0 )
+    {
+        salida = "#\(num)"
+    }
+    
+    print(salida)
+    
+}
